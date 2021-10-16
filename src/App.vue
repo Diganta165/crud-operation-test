8897e724ffebd668e602a365b3ab515b857ce289
Diganta165 <mdsrdiganta@gmail.com>
2021-10-16 16:05:09 +0600

    update component added but has some error

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <my-form :form="form" @onFormSubmit = "onFormSubmit"></my-form>
     <users-component :users = 'users' @onDelete = "onDelete"></users-component>
     <!-- <button @click="getUsers">Click</button> -->
   </div>
@@ -11,18 +12,21 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import axios from 'axios'
 import UsersComponent from './components/UsersComponent.vue'
+import MyForm from './components/MyFormComponent.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    UsersComponent
+    UsersComponent,
+    MyForm
   },
   data(){
     return{
       url: "https://jsonplaceholder.typicode.com/users",
-      users:[]
+      users:[],
+      form:{name:'', email:'', id:'', idEdit:false}
     }
   },
   methods:{
@@ -41,9 +45,29 @@ export default {
         console.log(err);
       })
     },
+    createUSer(){
+      axios.post(this.url,{
+        name:data.name,
+        email: data.email
+      }).then(()=>{
+        this.getUsers()
+      }
+      ).catch(err=>{
+        alert(err)
+      })
+    },
     onDelete(id){
       // console.log('Deleting from App vue', id)
       this.deleteUser(id)
+    },
+    onFormSubmit(data){
+      console.log(data)
+
+      if(data.isEdit){
+
+      }else{
+        this.createCustomer(data)
+      }
     }
   },
   created(){
---
+++ src/components/MyFormComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="name" :value="form.name" placeholder="Name" @change="handleChange" />
+      <input type="text" name="email" :value="form.email" placeholder="Email" @change="handleChange"/>
+      <!-- <input type="text" name="" placeholder="Name" /> -->
+      <button @click="onFormSubmit">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    form: {
+      type: Object,
+    },
+  },
+  methods:{
+      handleChange(event){
+          const {name, value} = event.target ;
+          let form = this.form;
+          form[name] = value;
+          this.form = form;
+
+      },
+      onFormSubmit(e){
+          e.preventDefault();
+
+          if(this.formValidation()){
+              console.log('ready to submit')
+              this.$emit("onFormSubmit", this.form)
+          }
+      },
+      formValidation(){
+          if(document.getElementsByName('name')[0].value === ""){
+              alert("Enter Name");
+              return false;
+          }
+          if(document.getElementsByName('email')[0].value === ""){
+              alert("Enter Email");
+              return false
+          }
+      }
+  }
+};
+</script>
+
+<style>
+</style>
